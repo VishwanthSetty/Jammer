44a219cef7e1b552ac455cfb92886bf67d195fdc
root <root@localhost.localdomain>
2019-01-30 01:45:59 +0530

    added The file

---
+++ jam.py
@@ -0,0 +1,14 @@
+#jam
+
+import sys,os
+import subprocess
+#subprocess.call(["python3","test.py"])
+#abc=subprocess.check_output(["python3","test.py"])
+print("abc")
+lists=subprocess.Popen(["python3","test.py"],stdout=subprocess.PIPE)
+try:
+	abc=lists.communicate(timeout=2)
+except subprocess.TimeoutExpired:
+	lists.kill()
+	abc=lists.communicate()
+print(abc)
